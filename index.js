300c4423a7d63b38e043881da3a57ffa53c5c122
alexanto222 <37571008+alexanto222@users.noreply.github.com>
2019-12-18 17:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const redis = require('redis');
 const process = require('process');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0);	
+  process.exit(2);
   client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
 
-app.listen(8081, () => {
-  console.log('Listening on port 8081');
+app.listen(6000, () => {
+  console.log('Listening on port 6000');
 });
